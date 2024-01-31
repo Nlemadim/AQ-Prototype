cd3c14730f2689ecfc95de8674e8ad7022b8402e
Nlemadim <nlemadimtony@gmail.com>
2024-01-31 00:23:42 -0500

    Full Screen Quiz View UI implementation

---
+++ AUdio Quiz Beta/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  AUdio Quiz Beta
+//
+//  Created by Tony Nlemadim on 1/30/24.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
